b08eb29c3a1090658de0e4374619f177a6abda42
mdislam2159 <mdsislam2159@gmail.com>
2020-07-04 20:01:37 -0400

    First Commit...

---
+++ src/test/java/com/google/GoogleSearch.java
@@ -0,0 +1,17 @@
+package com.google;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearch {
+	
+	public static void main(String[]args)
+	{
+		System.setProperty("webdriver.chrome.driver", ".\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com");
+	}
+
+
+}
